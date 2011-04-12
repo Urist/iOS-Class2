44d36a7a0bb23808f62f8ef44caaa0ee44f328cd
Joe Barcham <joebarcham@gmail.com>
2011-04-12 16:58:31 +1000

    Added View controller PeopleController

---
+++ Classes/PeopleController.h
@@ -0,0 +1,16 @@
+//
+//  PeopleController.h
+//  Class2
+//
+//  Created by Joe on 12/04/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface PeopleController : UITableViewController {
+
+}
+
+@end
